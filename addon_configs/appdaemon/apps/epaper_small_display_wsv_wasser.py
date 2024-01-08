877a27f1f83aba240eb670c3950e7bbe0e62e593
svenakela <sven.wesley@gmail.com>
2024-01-08 20:41:22 +0100

    Unique image names for all classes

--- addon_configs/appdaemon/apps/epaper_small_display_wsv_wasser.py
+++ addon_configs/appdaemon/apps/epaper_small_display_wsv_wasser.py
@@ -9,10 +9,12 @@ MAC = '0000021C6AB33B12'
 # The UUID in the URL below is found at https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations.json
 API_URL = 'https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/a6ee8177-107b-47dd-bcfd-30960ccc6e9c/W/measurements.json?start=P1D'
 LEVEL_NORMAL = 6.5
-LEVEL_WARNING = 7.8
+LEVEL_WARNING = 7.08
+CHART_LATEST_TO_RIGHT = True
+
 CHART_EVENT = 'EPAPER_GENERATE_CHART_WSV_WASSER'
 SECONDS_DELAY_TO_NOT_CHOKE_THE_POOR_AP = 90
-SEND_DATA_TO_HA_AS_SENSOR = True
+SEND_DATA_TO_HA_AS_SENSOR = False
 IMG_WIDTH = 296
 IMG_HEIGHT = 128
 
@@ -48,7 +50,9 @@ class SmallDisplayChartWsvWasser(Hass):
           return []
     
     def reduce_to_hourly_values(self, json_arr):
-        return [x['value']/100 for x in json_arr[-1::-4]]
+        reduced = [x['value']/100 for x in json_arr[-1::-4]]
+        if CHART_LATEST_TO_RIGHT: reduced.reverse()
+        return reduced
 
     def generate_chart(self, event, data, kwargs):
         CHART_WIDTH = IMG_WIDTH*0.741
@@ -70,13 +74,13 @@ class SmallDisplayChartWsvWasser(Hass):
         factor_x = round(CHART_WIDTH/len(VALUES))
 
         iterator_y = map(ppu_fraction, VALUES)
-        price_line = []
-        price_rect = []
+        value_line = []
+        value_rect = []
 
         for k,v in enumerate(iterator_y):
-          price_line.append((k*factor_x, v))
-          price_line.append((k*factor_x+factor_x, v))
-          price_rect.append([(k*factor_x, min(v, ppu_0)), (k*factor_x+factor_x, max(v, ppu_0))])
+          value_line.append((k*factor_x, v))
+          value_line.append((k*factor_x+factor_x, v))
+          value_rect.append([(k*factor_x, min(v, ppu_0)), (k*factor_x+factor_x, max(v, ppu_0))])
 
         image = Image.new('P', (IMG_WIDTH, IMG_HEIGHT))
         palette = [
@@ -92,11 +96,11 @@ class SmallDisplayChartWsvWasser(Hass):
         FONT_UPD = ImageFont.truetype('/config/apps/FreeMonoBold.ttf', size=10)
 
         # Colourize chart
-        for (pr, v) in zip(price_rect, VALUES):
+        for (pr, v) in zip(value_rect, VALUES):
           if v > LEVEL_WARNING:
             draw.rectangle(pr, fill=2)
 
-        draw.line(price_line, fill=1, width=4)
+        draw.line(value_line, fill=1, width=4)
 
         # x-lines
         y_val = -1
@@ -109,12 +113,14 @@ class SmallDisplayChartWsvWasser(Hass):
           y_val += 0.25 if ppu > 110 else 0.5 if ppu > 60 else 1
 
         # y-lines
-        for k,v in enumerate(price_line[::4]):
+        start_hour = HOUR_NOW-len(value_line[::2]) if CHART_LATEST_TO_RIGHT else HOUR_NOW
+        direction = 1 if CHART_LATEST_TO_RIGHT else -1
+        for k,v in enumerate(value_line[::4]):
           draw.line([(v[0],ppu_0-10), (v[0], ppu_0)], fill=1, width=1)
-          draw.text((v[0], ppu_0), f'{(HOUR_NOW-k*2)%24:02d}', font=FONT_STD, fill=1, stroke_width=1, stroke_fill=0)
+          draw.text((v[0], ppu_0), f'{(start_hour+(direction*k)*2)%24:02d}', font=FONT_STD, fill=1, stroke_width=1, stroke_fill=0)
 
         # y-line midnight
-        midnight = (HOUR_NOW)*factor_x
+        midnight = (24-HOUR_NOW)*factor_x if CHART_LATEST_TO_RIGHT else HOUR_NOW*factor_x
         mid_y = 0
         while mid_y < CHART_HEIGHT-5:
           draw.line([(midnight, mid_y), (midnight, mid_y+3)], fill=2, width=1)
@@ -126,7 +132,8 @@ class SmallDisplayChartWsvWasser(Hass):
         start_y = IMG_HEIGHT*0.04
         val_adj = 36
         lbl_adj = 20
-        draw.text((start_x, start_y), f'{VALUES[0]:.2f}', font=FONT_BIG, fill=fill(VALUES[0]))
+        last_val = VALUES[-1] if CHART_LATEST_TO_RIGHT else VALUES[0]
+        draw.text((start_x, start_y), f'{last_val:.2f}', font=FONT_BIG, fill=fill(last_val))
         draw.text((start_x, start_y+lbl_adj), f'@{HOUR_NOW:02d}', font=FONT_STD, fill=1)
         draw.text((start_x, start_y+val_adj), f'{max_value:.2f}', font=FONT_BIG, fill=fill(max_value))
         draw.text((start_x, start_y+val_adj+lbl_adj), f'max', font=FONT_STD, fill=1)
@@ -136,7 +143,7 @@ class SmallDisplayChartWsvWasser(Hass):
         draw.text((IMG_WIDTH-36, IMG_HEIGHT-12), f'{datetime.now().strftime("%H:%M")}', font=FONT_UPD, fill=1)
 
         rgb_image = image.convert('RGB')
-        image_path = 'display_small.jpg'
+        image_path = f'{CHART_EVENT}_display_small.jpg'
         rgb_image.save(image_path, 'JPEG', quality="maximum")
 
         url = "http://" + AP_IP + "/imgupload"
